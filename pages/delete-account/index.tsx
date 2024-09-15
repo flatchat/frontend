8eb27fbe1cf2506eae8d97bef164da6ae6235c73
singhalkarun <singhalkarun@gmail.com>
2024-09-15 20:10:31 +0530

    add a delete account page

---
+++ pages/delete-account/index.tsx
@@ -0,0 +1,10 @@
+export default function DeleteAccount() {
+    return (
+      <>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-[#2029BA]">
+          <h1>Reach out to contact@flatchat.app to delete account</h1>
+        </div>
+      </>
+    );
+  }
+  
